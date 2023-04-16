cb61c61f9a7c92e12698eea91e52c16cd2353855
f <f@email>
2023-04-16 23:27:11 +0200

    integrated feedback index page

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Convergent Facilitation";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
+  "Convergent Facilitation (CF) is a facilitated decision making process that helps groups come together and find solutions that work for everyone, even when there are divergent opinions and perspectives. It is a collaborative approach that emphasizes listening, understanding and empowering everyone to care for the whole.";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+export const MY_NAME = "Convergent Facilitation";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
